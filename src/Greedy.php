c24195f486ca3e7e353a96c964c81d02ed7aa354
Max Datsenko <datsenko.md@gmail.com>
2020-10-01 19:34:44 +0300

    add greedy

---
+++ src/Greedy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class Greedy
+{
+    public function search($stations, \Ds\Set $statesNeeded)
+    {
+        $finalStations = new \Ds\Set();
+        
+        while (!$statesNeeded->isEmpty()) {
+            $bestStation = null;
+            $statesCovered = new \Ds\Set();
+
+            foreach ($stations as $name => $states) {
+                $covered = $statesNeeded->intersect($states);
+                if ($covered->count() > $statesCovered->count()) {
+                    $bestStation = $name;
+                    $statesCovered = $covered;
+                }
+            }
+
+            $finalStations->add($bestStation);
+            $statesNeeded->remove(...$statesCovered->toArray());
+        }
+
+        return $finalStations;
+    }
+}
